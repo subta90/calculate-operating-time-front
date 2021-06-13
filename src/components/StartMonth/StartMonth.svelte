67311f4ba50bcd9a0d24a6d0258d34ae0ae79e28
subta90 <the.bird.nobility298@gmail.com>
2021-06-13 15:56:48 +0900

    開始ダイアログからメインコンポーネントに現在の月と、稼働予定日数を渡して表示

---
+++ src/components/StartMonth/StartMonth.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { getContext } from "svelte";
+    import { push } from "svelte-spa-router";
+    import StartMonthDialog from "./StartMonthDialog.svelte";
+    import { currentSelectedMonth, currentWorkingDays} from '../../stores/store.js';
+
+    const { open } = getContext("simple-modal");
+
+    const showStartMonth = () => {
+        open(StartMonthDialog, {
+            onOK: () => {
+                push("/main");
+            },
+        });
+    };
+</script>
+
+<p><button on:click={showStartMonth}>開始</button></p>
